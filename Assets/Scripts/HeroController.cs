e6786706c3517b3f154608364c7a28081d8bbdc2
qmzik <acke208@yandex.ru>
2018-03-03 00:38:38 +0500

    char was animated

--- Assets/Scripts/HeroController.cs
+++ Assets/Scripts/HeroController.cs
@@ -9,40 +9,40 @@ public class HeroController : MonoBehaviour
     public float JumpForce;
     //public AudioClip walk, run, jump, gotPoint, hit;
 
-
     //private AudioSource audioSource;
     private GameObject player;
     private Rigidbody2D rb;
-    //private Animator animator;
+    private Animator animator;
     private Direction direction = Direction.right;
     private bool isJumping = false;
     private float jumpCooldown = 0.5f;
     private bool isJumpCooldown = false;
 
-    /*private CharState State
+
+    private CharState State
     {
         get { return (CharState)animator.GetInteger("State"); }
         set { animator.SetInteger("State", (int)value); }
-    }*/
+    }
 
     void Start()
     {
         player = gameObject;
         rb = GetComponent<Rigidbody2D>();
-        //animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
         //audioSource = GetComponent<AudioSource>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
-       /* if(isJumping)
+        if(isJumping)
         {
             State = CharState.jump;
         }
         else
         {
             State = CharState.idle;
-        }*/
+        }
 
         if (Input.GetKey(KeyCode.D))
         {
@@ -50,10 +50,13 @@ public class HeroController : MonoBehaviour
             {
                 transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
             }
-
             direction = Direction.right;
-           // State = CharState.run;
-            MoveRight();
+
+            if (!isJumping)
+            {
+                State = CharState.run;
+            }
+                MoveRight();
         }
 
         if (Input.GetKey(KeyCode.A))
@@ -64,22 +67,17 @@ public class HeroController : MonoBehaviour
             }
 
             direction = Direction.left;
-            //State = CharState.run;
+
+            if (!isJumping)
+            {
+                State = CharState.run;
+            }
             MoveLeft();
         }
 
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space))
         {
             Jump();
-        }
-
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            Walkspeed = 7f;
-        }
-        else
-        {
-            Walkspeed = 5f;
         }
     }
 
@@ -95,18 +93,18 @@ public class HeroController : MonoBehaviour
 
     void Jump()
     {
-        //State = CharState.jump;
+        State = CharState.jump;
         if (!isJumping && !isJumpCooldown)
         {
             //audioSource.PlayOneShot(jump);
             isJumping = true;
             isJumpCooldown = true;
             rb.AddForce(new Vector2(0, JumpForce));
-            Invoke("CoolDown", jumpCooldown);
+            Invoke("JumpingCoolDown", jumpCooldown);
         }
     }
 
-    void CoolDown()
+    void JumpingCoolDown()
     {
         isJumpCooldown = false;
     }
