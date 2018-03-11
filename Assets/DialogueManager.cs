7c7f40b285dfbaa7747297e9565cfaea97d3cdca
Никита Лебедев <acke208@yandex.ru>
2018-03-11 22:52:13 +0500

    dialogue system is added

---
+++ Assets/DialogueManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    public Text nameText;
+    public Text dialogueText;
+    public GameObject dialoguePanel;
+
+    private HeroController heroController;
+    private Queue<string> sentences;
+
+	// Use this for initialization
+	void Start () {
+        sentences = new Queue<string>();
+        dialoguePanel.SetActive(false);
+        heroController = GameObject.FindGameObjectWithTag("Player").GetComponent<HeroController>();
+	}
+	
+	public void StartConversation(Dialogue dialogue)
+    {
+        heroController.enabled = false;
+        dialoguePanel.SetActive(true);
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndConversation();
+
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+    void EndConversation()
+    {
+        dialoguePanel.SetActive(false);
+        heroController.enabled = true;
+    }
+}
---
+++ Assets/PixelArt/Background/GrandmomHouse/StartDialog.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartDialog : MonoBehaviour {
+
+    public GameObject info;
+    public Dialogue dialogue;
+
+    bool canIStartDialogue = false;
+    bool isDialogueStarted = false;
+
+    private void Start()
+    {
+        info.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && canIStartDialogue && !isDialogueStarted)
+        {
+            info.SetActive(false);
+            FindObjectOfType<DialogueManager>().StartConversation(dialogue);
+            isDialogueStarted = true;
+        }
+
+        if(Input.GetKeyDown(KeyCode.F) && isDialogueStarted)
+        {
+            FindObjectOfType<DialogueManager>().DisplayNextSentence();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!isDialogueStarted)
+        {
+            info.SetActive(true);
+            canIStartDialogue = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        info.SetActive(false);
+        canIStartDialogue = false;
+    }
+}
