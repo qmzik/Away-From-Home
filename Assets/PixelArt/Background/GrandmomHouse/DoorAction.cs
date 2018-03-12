a360ff38ea52ac3f561ce3ec0bdc9a1092ac6e7e
myth3r <myth3rme@gmail.com>
2018-03-12 13:50:06 +0500

    Added door and bulb

---
+++ Assets/PixelArt/Background/GrandmomHouse/DoorAction.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class DoorAction : MonoBehaviour {
+
+    public GameObject info;
+    bool canIGo = false;
+
+	void Start () {
+        info.SetActive(false);
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        info.SetActive(true);
+        canIGo = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        info.SetActive(false);
+        canIGo = false;
+    }
+
+    void Update () {
+		if(Input.GetKeyDown(KeyCode.E) && canIGo)
+        {
+            Debug.Log("I leave, bye bye");
+        }
+	}
+}
